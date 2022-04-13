d4db148fa0c1ef70947c80598b1ebeb9ae3bfd52
Timur <tima950203@gmail.com>
2022-04-13 18:14:11 +0200

    check

--- src/Classes/Blockchain.ts
+++ src/Classes/Blockchain.ts
@@ -524,8 +524,8 @@ class Blockchain {
         })
         this._provider.on('chainChanged', async () => {
           this._networkId = await web3.eth.net.getId()
-          console.log('chainChanged triggered')
-          this.connectTo(AuthType.WALLET_CONNECT)
+          // console.log('chainChanged triggered')
+          // this.connectTo(AuthType.WALLET_CONNECT)
         })
         break
       case ProviderName['MEW wallet']:

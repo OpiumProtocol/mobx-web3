4f2b5ecdf702b9d4cd4dc3e4e19336dc154327d1
Timur <tima950203@gmail.com>
2022-04-13 17:20:47 +0200

    cehck

--- src/Classes/Blockchain.ts
+++ src/Classes/Blockchain.ts
@@ -518,7 +518,9 @@ class Blockchain {
         this.clientName = walletName
         this._log.info(this.clientName)
         this._provider.on('disconnect', () => {
+          console.log('disconnect triggered')
           localStorage.removeItem('WEB3_CONNECT_CACHED_PROVIDER')
+          console.log(localStorage.getItem('WEB3_CONNECT_CACHED_PROVIDER'))
         })
         this._provider.on('chainChanged', async () => {
           this._networkId = await web3.eth.net.getId()
